a930b3feb33d85a38ecb1d26cb41a090f6c331b0
Wesley McCammon <wesleymccammon@hotmail.com>
2026-06-03 10:42:42 -0500

    Added filtering

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { ForexService } from '../forex.service';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ForexPair } from '../forex-pair/forex-pair';
+import { ForexService } from '../forex.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [ForexPair],
+  imports: [ForexPair, NgIf],
   templateUrl: './home.html',
   styles: [
     `
@@ -36,4 +37,28 @@ import { ForexPair } from '../forex-pair/forex-pair';
 })
 export class Home {
   protected readonly quotes = inject(ForexService).quotes;
+
+  protected readonly filterType = signal<'all' | 'base' | 'quote'>('all');
+  protected readonly selectedCurrency = signal('');
+
+  protected readonly currencyOptions = computed(() => {
+    const type = this.filterType();
+    if (type === 'all') {
+      return [] as string[];
+    }
+
+    const index = type === 'base' ? 0 : 1;
+    return [...new Set(this.quotes().map((quote) => quote.pair.split('/')[index]))].sort();
+  });
+
+  protected readonly filteredQuotes = computed(() => {
+    const type = this.filterType();
+    const currency = this.selectedCurrency().trim();
+    if (type === 'all' || !currency) {
+      return this.quotes();
+    }
+
+    const index = type === 'base' ? 0 : 1;
+    return this.quotes().filter((quote) => quote.pair.split('/')[index] === currency);
+  });
 }
